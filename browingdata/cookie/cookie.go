2c535ebbb2ce46a0a8200ab3f355657722b3ff60
hungdhv97 <hungdhv97@gmail.com>
2023-06-11 01:18:51 +0700

    Update cookie.go

--- browingdata/cookie/cookie.go
+++ browingdata/cookie/cookie.go
@@ -2,9 +2,10 @@ package cookie
 
 import (
 	"database/sql"
+	"net/url"
 	"os"
 	"sort"
-	"time"
+	"strings"
 
 	// import sqlite3 driver
 	_ "github.com/mattn/go-sqlite3"
@@ -18,17 +19,17 @@ import (
 type ChromiumCookie []chrome_cookie
 
 type chrome_cookie struct {
-	host_key        string
-	path            string
-	name            string
-	encrypted_value []byte
-	value           string
-	is_secure       bool
-	is_httponly     bool
-	has_expires     bool
-	is_persistent   bool
-	creation_utc    int64
-	expires_utc     int64
+	HostKey        string `json:"host_key" csv:"host_key"`
+	Path           string `json:"path" csv:"path"`
+	Name           string `json:"name" csv:"name"`
+	EncryptedValue []byte `json:"encrypted_value" csv:"encrypted_value"`
+	Value          string `json:"value" csv:"value"`
+	IsSecure       bool   `json:"is_secure" csv:"is_secure"`
+	IsHTTPOnly     bool   `json:"is_httponly" csv:"is_httponly"`
+	HasExpires     bool   `json:"has_expires" csv:"has_expires"`
+	IsPersistent   bool   `json:"is_persistent" csv:"is_persistent"`
+	CreationUTC    int64  `json:"creation_utc" csv:"creation_utc"`
+	ExpiresUTC     int64  `json:"expires_utc" csv:"expires_utc"`
 }
 
 const (
@@ -59,16 +60,16 @@ func (c *ChromiumCookie) Parse(masterKey []byte) error {
 		}
 
 		cookie := chrome_cookie{
-			name:            key,
-			host_key:        host,
-			path:            path,
-			encrypted_value: encryptValue,
-			is_secure:       typeutil.IntToBool(isSecure),
-			is_httponly:     typeutil.IntToBool(isHTTPOnly),
-			has_expires:     typeutil.IntToBool(hasExpire),
-			is_persistent:   typeutil.IntToBool(isPersistent),
-			creation_utc:    createDate,
-			expires_utc:     expireDate,
+			Name:           key,
+			HostKey:        host,
+			Path:           path,
+			EncryptedValue: encryptValue,
+			IsSecure:       typeutil.IntToBool(isSecure),
+			IsHTTPOnly:     typeutil.IntToBool(isHTTPOnly),
+			HasExpires:     typeutil.IntToBool(hasExpire),
+			IsPersistent:   typeutil.IntToBool(isPersistent),
+			CreationUTC:    createDate,
+			ExpiresUTC:     expireDate,
 		}
 		if len(encryptValue) > 0 {
 			if len(masterKey) == 0 {
@@ -80,11 +81,16 @@ func (c *ChromiumCookie) Parse(masterKey []byte) error {
 				log.Error(err)
 			}
 		}
-		cookie.value = string(value)
+		cookie.Value = string(value)
 		*c = append(*c, cookie)
 	}
 	sort.Slice(*c, func(i, j int) bool {
-		return time.Unix((*c)[i].creation_utc, 0).After(time.Unix((*c)[j].creation_utc, 0))
+		domainI := parseDomain((*c)[i].HostKey)
+		domainJ := parseDomain((*c)[j].HostKey)
+		if domainI == domainJ {
+			return (*c)[i].HostKey < (*c)[j].HostKey
+		}
+		return domainI < domainJ
 	})
 	return nil
 }
@@ -100,14 +106,14 @@ func (c *ChromiumCookie) Len() int {
 type FirefoxCookie []firefox_cookie
 
 type firefox_cookie struct {
-	name         string
-	host         string
-	path         string
-	isSecure     bool
-	isHttpOnly   bool
-	creationTime int64
-	expiry       int64
-	value        string
+	Name         string `json:"name" csv:"name"`
+	Host         string `json:"host" csv:"host"`
+	Path         string `json:"path" csv:"path"`
+	IsSecure     bool   `json:"is_secure" csv:"is_secure"`
+	IsHTTPOnly   bool   `json:"is_httponly" csv:"is_httponly"`
+	CreationTime int64  `json:"creation_time" csv:"creation_time"`
+	Expiry       int64  `json:"expiry" csv:"expiry"`
+	Value        string `json:"value" csv:"value"`
 }
 
 const (
@@ -137,19 +143,24 @@ func (f *FirefoxCookie) Parse(_ []byte) error {
 			log.Warn(err)
 		}
 		*f = append(*f, firefox_cookie{
-			name:         name,
-			host:         host,
-			path:         path,
-			isSecure:     typeutil.IntToBool(isSecure),
-			isHttpOnly:   typeutil.IntToBool(isHTTPOnly),
-			creationTime: creationTime,
-			expiry:       expiry,
-			value:        value,
+			Name:         name,
+			Host:         host,
+			Path:         path,
+			IsSecure:     typeutil.IntToBool(isSecure),
+			IsHTTPOnly:   typeutil.IntToBool(isHTTPOnly),
+			CreationTime: creationTime,
+			Expiry:       expiry,
+			Value:        value,
 		})
 	}
 
 	sort.Slice(*f, func(i, j int) bool {
-		return time.Unix((*f)[i].creationTime, 0).After(time.Unix((*f)[j].creationTime, 0))
+		domainI := parseDomain((*f)[i].Host)
+		domainJ := parseDomain((*f)[j].Host)
+		if domainI == domainJ {
+			return (*f)[i].Host < (*f)[j].Host
+		}
+		return domainI < domainJ
 	})
 	return nil
 }
@@ -160,4 +171,16 @@ func (f *FirefoxCookie) Name() string {
 
 func (f *FirefoxCookie) Len() int {
 	return len(*f)
+}
+
+func parseDomain(host string) string {
+	u, err := url.Parse("http://" + host)
+	if err != nil {
+		return host
+	}
+	parts := strings.Split(u.Hostname(), ".")
+	if len(parts) >= 2 {
+		return parts[len(parts)-2] + "." + parts[len(parts)-1]
+	}
+	return u.Hostname()
 }
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/urfave/cli/v2 v2.25.0
 	golang.org/x/crypto v0.7.0
 	golang.org/x/exp v0.0.0-20230310171629-522b1b587ee0
-	golang.org/x/text v0.8.0
+	golang.org/x/text v0.9.0
 )
 
 require (
@@ -32,6 +32,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/net v0.10.0
+	golang.org/x/sys v0.8.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
